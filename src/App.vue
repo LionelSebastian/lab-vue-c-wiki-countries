d07496b7dcfb59a33899c847742eea5337442a6b
LionelSebastian <lionel.sebastian.cifo@gmail.com>
2022-12-08 16:54:14 +0100

    Fixing => Components name, importing sountries json, created html components yet without fetching data from json

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="app">
-
+  <div class="">
+    <NavBar/>
+    <CountriesList/>   
   </div>
 </template>
 
 <script>
+import CountriesList from './components/CountriesList.vue';
+import NavBar from './components/NavBar.vue';
+import COUNTRIES from '../public/countries.json';
 
+export default{ 
+    components:{
+      NavBar,
+      CountriesList,    
+      },
+    data(){
+      countries:COUNTRIES;
+    },
+};
 </script>
 
 <style>
-
 </style>
